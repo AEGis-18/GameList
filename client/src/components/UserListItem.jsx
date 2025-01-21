6e4c81af473c4cb4c1b819d85cc8b5169e8e222e
AEGis-18 <agusesgi@gmail.com>
2025-01-21 16:47:50 -0300

    Fix User List games

--- client/src/components/UserListItem.jsx
+++ client/src/components/UserListItem.jsx
@@ -4,10 +4,10 @@ export default function UserListItem({ userList, userGames }) {
   return (
     <>
       <h1 className="text-2xl font-bold mb-4 px-4">User Games</h1>
-
+      {console.log("usergames: ", userGames)}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
         {userList.map((item) => {
-          const gameInfo = userGames[item.game];
+          const gameInfo = userGames.find((game) => game[0].id === item.game);
           console.log("elemento", gameInfo ? gameInfo[0] : "");
           return (
             <div
